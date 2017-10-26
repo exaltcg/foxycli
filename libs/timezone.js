7cd9b81b89b75f91cd5d5b01c64e6f6aa51fc700
Nikolay Galkin <nikolay@exaltcg.com>
2017-10-26 15:55:03 +0300

    Get Local time for Weather

---
+++ libs/timezone.js
@@ -0,0 +1,23 @@
+const request = require('request-promise');
+
+const GOOGLE_API = 'AIzaSyA5W_Ilofq13teL2EDRclCPIbsjZTPyUlM';
+
+class Timezone {
+
+  getLocaltime(coords) {
+    const now = new Date();
+    const location = `${coords.lat} ${coords.lon}`;
+    const timestamp = Math.floor(now.getTime() / 1000);
+    const url = `https://maps.googleapis.com/maps/api/timezone/json?location=${location}&timestamp=${timestamp}&key=${GOOGLE_API}`;
+    return request(url).then(response => {
+      const data = JSON.parse(response);
+      console.log(data.rawOffset);
+      const utc = now.getTime() + now.getTimezoneOffset() * 60000;
+      const date = new Date(utc + data.rawOffset * 1000);
+      const hours = ('0' + date.getHours()).slice(-2);
+      return `${hours}:${date.getMinutes()}`;
+    });
+  }
+}
+
+module.exports = new Timezone;
